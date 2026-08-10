abeb4066ccf220f163bd03e888a57c894a209b3f
Matheus Junqueira Jundurian Bolonha <tejunqueira22@gmail.com>
2026-08-10 12:57:30 -0300

    feat: footer implementado

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#1C2E36] py-4 text-white">
+      <div className="container mx-auto flex items-center justify-center gap-3">
+        <p>Desenvolvido por</p>
+        <div className="max-w-50">
+          <img src="/img/logo_byron.png" alt="Logo da byronSolutions" />
+        </div>
+      </div>
+    </footer>
+  );
+}
