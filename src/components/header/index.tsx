d91ca14d9ecbfcb997cefdffacb96e09654554cf
Matheus Junqueira Jundurian Bolonha <tejunqueira22@gmail.com>
2026-08-11 20:40:53 -0300

    fix: ajustando rotas do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,24 +7,26 @@ export default function Header() {
     <header className="w-full z-20 flex items-center justify-between px-8 py-4 bg-gradient-to-b from-[#1C2E36] to-[#000000]">
       
       {/* Logo do site */}
-      <div className="relative w-12 h-12">
-        <Image
-          src="/img/logo.png"
-          alt="Logo CrossForce no Header"
-          fill
-          className="object-contain"
-        />
+      <div className="relative w-12 h-12 hover:scale-125 transition-all" >
+        <Link href="/">
+          <Image
+            src="/img/logo.png"
+            alt="Logo CrossForce no Header"
+            fill
+            className="object-contain"
+          />
+        </Link>
       </div>
 
       <nav>
         <ul className="flex gap-8 text-white font-medium">
-          <li>
+          <li className="hover:scale-125 transition-all">
             <Link href="/sobrePage/">Sobre</Link>
           </li>
-          <li>
+          <li className="hover:scale-125 transition-all">
             <Link href="/planosPage/">Planos</Link>
           </li>
-          <li>
+          <li className="hover:scale-125 transition-all">
             <Link href="/">Professores</Link>
           </li>
         </ul>
