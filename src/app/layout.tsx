6bf93b6bcabd690014a15ead9b3545ad5cf85e35
Eduardo Lima <138487802+Dudruu@users.noreply.github.com>
2026-08-18 13:59:24 -0300

    feat: implemetação do lenin, configurações iniciais e implementação de uma rolagem suave na página

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/src/components/header";
 import Footer from "../components/footer";
+import SmoothScroll from "../components/smoothScroll";
+
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -20,7 +22,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html lang="pt-BR" className={`${montserrat.variable} antialiased`}>
       <body className="min-h-full flex flex-col">
         <Header />
-        <main className="flex-1">{children}</main>
+        <SmoothScroll>
+          <main className="flex-1">{children}</main>
+        </SmoothScroll>
         <Footer/>
       </body>
     </html>
